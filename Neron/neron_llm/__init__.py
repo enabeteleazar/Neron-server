e759966e8714e69317971c6787220a00c54efb29
enabeteleazar <nabet.eleazar@gmail.com>
2026-01-24 16:54:25 +0000

    update

--- Neron/neron_llm/__init__.py
+++
@@ -1,6 +0,0 @@
-from .llm_core import LLMCore
-from .llm_manager import LLMManager
-from .prompt_handler import PromptHandler
-
-
-__all__ = ["LLMCore", "LLMManager", "PromptHandler"]
--- Neron/neron_llm/config.py
+++
@@ -1,6 +0,0 @@
-# Nom du modele Ollama
-MODEL_NAME = "llama3:2-1b"
-
-# Parametre de generation
-MAX_TOKENS = 512
-TEMPERATURE = 0.7
