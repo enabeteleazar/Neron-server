bd19683274d7ff61691375ccb60531c6b02fa8b7
enabeteleazar <nabet.eleazar@gmail.com>
2026-04-10 08:22:16 +0200

    install.sh master-release

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ INSTALL_DIR="${NERON_DIR:-/etc/neron}"
 if [ -f "${NERON_DIR:-/etc/neron}/.env" ]; then
     _BRANCH=$(grep "^NERON_BRANCH" "${NERON_DIR:-/etc/neron}/.env" 2>/dev/null | cut -d= -f2)
 fi
-BRANCH="${_BRANCH:-${NERON_BRANCH:-master}}"
+BRANCH="${_BRANCH:-${NERON_BRANCH:-release}}"
 
 # --- Configuration Telegram ---
 setup_telegram() {
